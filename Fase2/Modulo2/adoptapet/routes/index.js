29ff36e238363f1701f1559913bbf08e895fd686
14030598 <14030598@itcelaya.edu.mx>
2021-09-23 17:13:58 -0500

    auth en usuarios corregido

--- Fase2/Modulo2/adoptapet/routes/index.js
+++ Fase2/Modulo2/adoptapet/routes/index.js
@@ -1,10 +1,10 @@
 var router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('welcome to adoptapet');
-});
+	res.send('welcome to my api');
+})
 
-// router.use('/usuarios', require('./usuarios'));
+router.use('/usuarios', require('./usuarios'));
 router.use('/mascotas', require('./mascotas'));
 router.use('/solicitudes', require('./solicitudes'));
 
